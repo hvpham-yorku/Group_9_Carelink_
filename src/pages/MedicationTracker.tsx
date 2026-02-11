7e4ba24afad35c4ef4d1c4226c0144672abd6bf3
saneeakhal <saneeakhalid@gmail.com>
2026-02-10 22:26:09 -0500

    Add MedicationScheduleItem in MedicationTracter page

--- src/pages/MedicationTracker.tsx
+++ src/pages/MedicationTracker.tsx
@@ -1,9 +1,28 @@
+import MedicationScheduleItem from "../components/medication/MedicationScheduleItem";
+
 const MedicationTracker = () => {
+    const todaySchedule = [
+  { id: 1, name: "Metformin", dosage: "500mg", time: "08:00 AM" },
+  { id: 2, name: "Lisinopril", dosage: "10mg", time: "08:00 AM" },
+  { id: 3, name: "Aspirin", dosage: "81mg", time: "08:00 AM" },
+];
+
   return (
     <>
       <div className="container">
-        <h1>Medication Tracker</h1>
-      </div>
+  <h1>Medication Tracker</h1>
+
+  <div style={{ marginTop: "20px" }}>
+    {todaySchedule.map((med) => (
+      <MedicationScheduleItem
+        key={med.id}
+        name={med.name}
+        dosage={med.dosage}
+        time={med.time}
+      />
+    ))}
+  </div>
+</div>
     </>
   );
 };
