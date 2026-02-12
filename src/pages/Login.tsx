c9b9fadde9cd3ae4964f3bb224167faf7117c78c
Adeena Ahmed <yorkuni2@my.yorku.ca>
2026-02-11 20:36:22 -0500

    Adds all components to the Login page component: Login message, textboxes and button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import LoginText from "/Users/aa/Desktop/Git Repos/CareLink_Adeena/src/components/login/LoginText.tsx";
+import LoginTextBox from "/Users/aa/Desktop/Git Repos/CareLink_Adeena/src/components/login/LoginTextBox.tsx";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -6,6 +8,9 @@ const Login = () => {
   return (
     <>
       <div className="d-flex justify-content-center">
+       <LoginText/>
+       <LoginTextBox name="username" id="username" />
+       <LoginTextBox name="password" id="password" />
         <button
           className="btn btn-primary mt-5"
           onClick={() => navigate("/dashboard")}
