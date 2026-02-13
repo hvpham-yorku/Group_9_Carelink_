1e40fc0c4f192e46b1beee8d8bd15476a9a300f1
Adeena Ahmed <yorkuni2@my.yorku.ca>
2026-02-12 21:35:47 -0500

    Creates the test for the LoginTextBox component

--- src/test/LoginPage.test.tsx
+++ src/test/LoginPage.test.tsx
@@ -6,6 +6,8 @@ import '@testing-library/jest-dom';
 
 import { render, screen } from "@testing-library/react";
 import LoginText from "../components/login/LoginText";
+import LoginTextBox from "../components/login/LoginTextBox";
+
 
 /*
     Tests whether the LoginText component exists and renders correctly.
@@ -20,6 +22,32 @@ describe("LoginText", () => {
     });
 
     expect(heading).toBeInTheDocument();
+  });
+});
+
+/*
+
+    This tests the LoginTextBox component. It tests whether the component renders, the props were passed correctly,
+    and that the attributes were applied correctly.
+    
+*/
+
+describe("LoginTextBox", () => {
+  it("renders an input with correct attributes", () => {
+    render(
+      <LoginTextBox
+        name="username"
+        id="username"
+        placeholder="Enter username"
+      />
+    );
+
+    const input = screen.getByPlaceholderText(/enter username/i);
+
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute("name", "username");
+    expect(input).toHaveAttribute("id", "username");
+    expect(input).toHaveAttribute("type", "text");
   });
 });
 
