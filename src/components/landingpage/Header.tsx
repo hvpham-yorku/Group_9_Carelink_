adbba8524b6a7d78784eb85946afd53cdefdd8a4
Adeena Ahmed <yorkuni2@my.yorku.ca>
2026-03-02 10:01:18 -0500

    Creates the basic Header component

---
+++ src/components/landingpage/Header.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+  const navigate = useNavigate();
+  return (
+    <header className="header">
+      <div className="logo-container">
+        {/* Use the imported logo variable in the src attribute */}
+        <h1>CareLink</h1>
+      </div>
+      <nav className="nav-links">
+        {/* Use <a> tags for simple anchors or a routing library for navigation */}
+        <a href="#home">Features</a>
+        <a href="#about">Benefits</a>
+        <a href="#contact">Who It's For</a>
+        <button
+        className="btn btn-outline-primary btn-lg mt-5"
+        onClick={() => navigate("/Login")}
+        > Login
+        </button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
