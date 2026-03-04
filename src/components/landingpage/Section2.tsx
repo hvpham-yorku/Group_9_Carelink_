9b5bc39d978e6c7fde727c5dfd26689a7fd2712b
Adeena Ahmed <yorkuni2@my.yorku.ca>
2026-03-04 15:11:36 -0500

    Creates the Section2 component of the landing page

---
+++ src/components/landingpage/Section2.tsx
@@ -0,0 +1,22 @@
+
+export default function Section2() {
+    return (
+        <section className="bg-light">
+            <div className="row justify-content-center text-center p-5">
+                <div className="col-6 col-lg-6 col-md-8">
+                    <h2>Home Care Needs Better Tools</h2>
+                    <h5 className="text-secondary fw-light">
+                    Large hospital EHR systems are too complex. 
+                    Spreadsheets and paper notes lead to missed care actions. 
+                    CareLink is intentionally task-focused and optimized for 
+                    home care environments where caregivers work rotating shifts, 
+                    manage multiple patients, and rely on clear communication to 
+                    ensure patient safety.
+                    </h5>
+                </div>
+                
+            </div>
+            
+        </section>
+    )
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,14 @@
 import Footer from "../components/landingpage/Footer";
 import Header from "../components/landingpage/Header";
 import Hero from "../components/landingpage/Hero";
+import Section2 from "../components/landingpage/Section2";
 
 const LandingPage = () => {
   return (
     <>
     <Header />
     <Hero />
+    <Section2 />
     <Footer />
     </>
   );
