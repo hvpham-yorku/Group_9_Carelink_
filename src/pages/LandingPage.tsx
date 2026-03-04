7d25853b0f0968b0ec88a5c6eed2ff2c5ef19098
Adeena Ahmed <yorkuni2@my.yorku.ca>
2026-03-04 15:32:42 -0500

    Creates Section 6 of the Landing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import Footer from "../components/landingpage/Footer";
 import Header from "../components/landingpage/Header";
 import Hero from "../components/landingpage/Hero";
 import Section2 from "../components/landingpage/Section2";
+import Section6 from "../components/landingpage/Section6";
 
 const LandingPage = () => {
   return (
@@ -9,6 +10,7 @@ const LandingPage = () => {
     <Header />
     <Hero />
     <Section2 />
+    <Section6 />
     <Footer />
     </>
   );
