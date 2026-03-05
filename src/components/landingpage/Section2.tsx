afa7df8a611e5dc478e8bc7d3e9feb5a9e3dba27
Adeena Ahmed <yorkuni2@my.yorku.ca>
2026-03-05 12:27:16 -0500

    Edits Section 2's spacing and alignment

--- src/components/landingpage/Section2.tsx
+++ src/components/landingpage/Section2.tsx
@@ -3,9 +3,9 @@ export default function Section2() {
     return (
         <section className="bg-light">
             <div className="row justify-content-center text-center p-5">
-                <div className="col-6 col-lg-6 col-md-8">
+                <div className="col-11 col-md-8 col-lg-5 my-3">
                     <h2>Home Care Needs Better Tools</h2>
-                    <h5 className="text-secondary fw-light">
+                    <h5 className="text-secondary fw-light fs-5 lh-base">
                     Large hospital EHR systems are too complex. 
                     Spreadsheets and paper notes lead to missed care actions. 
                     CareLink is intentionally task-focused and optimized for 
