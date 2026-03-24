204bb72d9d8a8e69ef8e7d8053135fe1f7f4cce6
TheGhostly41 <theghostly41@gmail.com>
2026-03-24 18:29:26 -0400

    NoteRepo file added

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,6 +2,8 @@ import { StubTeamRepo } from "./repositories/teams/StubTeamRepo";
 import { ApiTeamRepo } from "./repositories/teams/ApiTeamRepo";
 import { ApiTaskRepo } from "./repositories/tasks/ApiTaskRepo";
 import { StubTaskRepo } from "./repositories/tasks/StubTaskRepo";
+import { StubPatientRepo } from "./repositories/patients/StubPatientRepo";
+import { ApiPatientRepo } from "./repositories/patients/ApiPatientRepo";
 
 // import.meta.env.VITE_STUB_MODE
 const mode = import.meta.env.VITE_STUB_MODE;
@@ -9,4 +11,5 @@ const mode = import.meta.env.VITE_STUB_MODE;
 export const repositories = {
   team: mode === "stub" ? new StubTeamRepo() : new ApiTeamRepo(),
   task: mode === "stub" ? new StubTaskRepo() : new ApiTaskRepo(),
+  patient: mode === "stub" ? new StubPatientRepo() : new ApiPatientRepo(),
 };
